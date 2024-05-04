f5ebaad2915b426af88506a328fe963461d89f90
minarhossain <codewithmnr@gmail.com>
2024-05-04 23:37:53 +0600

    Dynamic Task load with default

--- src/TaskBoard.jsx
+++ src/TaskBoard.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchTask from "./task/SearchTask";
 import TaskAction from "./task/TaskAction";
 import TaskList from "./task/TaskList";
+const defaultTask = {
+  id: crypto.randomUUID(),
+  title: "Integration API",
+  description:
+    "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+  tags: ["Web", "Python", "API", "Next"],
+  priority: "High",
+  isFavorite: true,
+};
 
 const TaskBoard = () => {
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20 flex justify-center" id="tasks">
       <div className="container">
@@ -11,7 +22,7 @@ const TaskBoard = () => {
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
--- src/task/TaskAction.jsx
+++ src/task/TaskAction.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const TaskAction = () => {
+const TaskAction = ({ onAdd }) => {
   return (
     <div className="mb-14 items-center justify-between sm:flex">
       <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
       <div className="flex items-center space-x-5">
-        <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
+        <button
+          onClick={onAdd}
+          className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold"
+        >
           Add Task
         </button>
         <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
