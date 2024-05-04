7277c97b2b6ada6d32b5cbae060fc958bf5ea66c
minarhossain <codewithmnr@gmail.com>
2024-05-04 14:47:47 +0600

    [task - management] starting

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./TaskBoard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <TaskBoard />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
