192e4885b0af0854234f60160de575da6fcfa720
minarhossain <codewithmnr@gmail.com>
2024-05-05 18:48:37 +0600

    modal open fon addtask click

--- src/TaskBoard.jsx
+++ src/TaskBoard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SearchTask from "./task/SearchTask";
 import TaskAction from "./task/TaskAction";
 import TaskList from "./task/TaskList";
+import AddTaskModal from "./task/AddTaskModal";
 const defaultTask = {
   id: crypto.randomUUID(),
   title: "Integration API",
@@ -14,14 +15,22 @@ const defaultTask = {
 
 const TaskBoard = () => {
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showModal, setShowModal] = useState(false);
+
+  // Add Task function
+
+  const handleAddTask = () => {
+    console.log("You clicked the button for task");
+  };
 
   return (
     <section className="mb-20 flex justify-center" id="tasks">
+      {showModal && <AddTaskModal />}
       <div className="container">
         <SearchTask />
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
+          <TaskAction onAddTask={() => setShowModal(true)} />
           <TaskList tasks={tasks} />
         </div>
       </div>
